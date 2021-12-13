0e6bbe9177c50a01229d41e17587d8f849c14523
Aysenurgnl <aysenurgunall50@gmail.com>
2021-12-13 19:14:32 +0300

    Initial Commit

---
+++ foodzilla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodzilla
+//
+//  Created by Ayşenur on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
